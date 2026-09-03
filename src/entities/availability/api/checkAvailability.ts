8717125bb9970905700175cda92e730259d185a1
Puloy <sequenajl@gmail.com>
2026-09-03 10:07:17 +0800

    set delay for race condition exercise 6

--- src/entities/availability/api/checkAvailability.ts
+++ src/entities/availability/api/checkAvailability.ts
@@ -3,14 +3,30 @@ import type {
   AvailabilityResponse,
 } from '../model/availability.types'
 
+// export function checkAvailability(
+//   request: AvailabilityRequest
+// ): Promise<AvailabilityResponse> {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve({
+//         available: request.employeeId % 2 === 0, // make available if id is even number (fake API)
+//       })
+//     }, 1000)
+//   })
+// }
+
 export function checkAvailability(
   request: AvailabilityRequest
 ): Promise<AvailabilityResponse> {
   return new Promise((resolve) => {
+    const delay = request.employeeId % 2 === 0
+      ? 2000
+      : 500
+
     setTimeout(() => {
       resolve({
-        available: request.employeeId % 2 === 0, // make available if id is even number (fake API)
+        available: request.employeeId % 2 === 0,
       })
-    }, 1000)
+    }, delay)
   })
 }
