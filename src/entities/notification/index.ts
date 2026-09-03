54e2932e4c01d62beba913142973c581394a0c47
Puloy <sequenajl@gmail.com>
2026-09-03 14:21:26 +0800

    notify user on success or failed service

---
+++ src/entities/notification/index.ts
@@ -0,0 +1,6 @@
+export type {
+    ShiftNotificationRequest,
+    ShiftNotificationResponse
+} from './model/notification.types'
+
+export { sendShiftNotification } from './api/sendShiftNotification'
